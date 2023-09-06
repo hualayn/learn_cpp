870a719fbf823c58aa8c3cd3128e14c851497f3f
layn <witity@hotmail.com>
2023-09-06 08:21:04 +0800

    update

---
+++ updater.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <direct.h>
+# include <string>
+
+using namespace std;
+
+int main() {
+    char _test_str[] = "this is a test str.";
+    cout << &_test_str << endl;
+    return 0;
+}
+
